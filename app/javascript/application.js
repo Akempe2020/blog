9ba66b87843015317808717a9577fe8462f16dac
akempe2020@gmail.com <akempe2020@gmail.com>
2024-06-25 15:44:07 -0400

    Improved looks

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+
+document.addEventListener("DOMContentLoaded", function() {
+    const items = document.querySelectorAll(".animated-list li");
+    
+    items.forEach((item, index) => {
+        item.style.animationDelay = `${(index + 1) * 0.3}s`; // Adjust delay as needed
+    });
+});
